a642d66d202ce8b61748917ccd7b3bd223268c1b
Lucas <thegreatberenger@gmail.com>
2024-02-26 16:23:01 -0300

    feat: create client model and database admin user

--- website/models.py
+++ website/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Cliente(models.Model):
+    nome = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False
+    )
+
+    email = models.EmailField(
+        blank=False,
+        null=False
+    )
+
+    telefone = models.IntegerField(
+        blank=False,
+        null=False
+    )
+
+    mensagem = models.CharField(
+        max_length=280
+    )
+
+    def __str__(self):
+        return self.nome
