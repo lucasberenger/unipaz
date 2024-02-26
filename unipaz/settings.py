18e96066d4ec213a39637020259989f37ffe91b4
Lucas <thegreatberenger@gmail.com>
2024-02-26 16:57:34 -0300

    feat: create send_email funcionalitie

--- unipaz/settings.py
+++ unipaz/settings.py
@@ -122,3 +122,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+##Email settings
+
+EMAIL_BACKEND = 'django.core.mail.backend.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'meuemail@gmail.com'
+EMAIL_HOST_PASSWORD = 'senha'
--- website/views.py
+++ website/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+import json
+from .models import Cliente
 
-# Create your views here.
+
+@csrf_exempt
+@require_POST
+def criar_cliente(request):
+    dados_cliente = json.loads(request.body)
+
+    novo_cliente = Cliente.objects.create(
+        nome=dados_cliente['nome'],
+        emial=dados_cliente['email'],
+        telefone=dados_cliente['telefone'],
+        mensagem=dados_cliente['mensagem']
+    )
+
+    assunto = 'Nova Mensagem de um cliente'
+    mensagem = f'Nome: {dados_cliente['nome']}\nEmail: {dados_cliente['email']}\ntelefone: {dados_cliente['telefone']}\nMensagem: {dados_cliente['mensagem']}'
+
+    destinatario = 'lucas@sjoseph.com.br'
+
+    send_mail(assunto, mensagem, destinatario, fail_silently=False)
+
+    return JsonResponse({'mensagem': 'Mensagem enviada com sucesso!'})
