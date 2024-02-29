7478b880dc81b05d5c965375036816dc17bf092a
Thiago <thberenger27@gmail.com>
2024-02-29 00:35:13 -0300

    att
     - clients animation

--- templates/website/static/js/script.js
+++ templates/website/static/js/script.js
@@ -17,7 +17,7 @@ barsButton.addEventListener('click', () => {
     }
 })
 
-// scroll animation
+// scroll window animation
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
@@ -32,3 +32,4 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden')
 hiddenElements.forEach((el) => observer.observe(el))
+
