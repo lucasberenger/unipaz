fe8919704e5ee28729d29215735373595a03d98d
Thiago <thberenger27@gmail.com>
2024-03-05 23:20:50 -0300

    att
     - responsive

--- website/static/js/script.js
+++ website/static/js/script.js
@@ -81,8 +81,8 @@ redirectAndScroll(eventButton, '#eventos');
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 2,
           autoplay: true,
           dots: false,
           autoplaySpeed: 2000,
