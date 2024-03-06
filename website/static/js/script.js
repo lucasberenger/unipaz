a71ba3f87317d9b51cc23428ba778f0808bc0467
Thiago <thberenger27@gmail.com>
2024-03-05 22:12:15 -0300

    att
     -responsive services

--- website/static/js/script.js
+++ website/static/js/script.js
@@ -63,11 +63,51 @@ redirectAndScroll(eventButton, '#eventos');
 
 // logo animation
 
-$('.clients-logo').slick({
-    slidesToShow: 5,
+// $('.clients-logo').slick({
+//     slidesToShow: 5,
+//     slidesToScroll: 4,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//   });
+
+  $('.clients-logo').slick({
+    dots: false,
+    speed: 300,
+    slidesToShow: 4,
     slidesToScroll: 4,
     autoplay: true,
     autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          autoplay: true,
+          dots: false,
+          autoplaySpeed: 2000,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          autoplay: true,
+          dots: false,
+          autoplaySpeed: 2000,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 2000,
+        }
+      }
+    ]
   });
 
 //  ----------
