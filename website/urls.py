ed3eca4ab2f342f6e1609b9f7c2fe6fc94598982
Lucas <thegreatberenger@gmail.com>
2024-03-09 02:15:25 -0300

    add send email method to the form and its validated logic.

--- website/urls.py
+++ website/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import HomePageView, CompanyServicesView, ContactView, EventsView
+from . import views
 
-app_name = "website"
+app_name = 'website'
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='index'),
-    path('servicos/', CompanyServicesView.as_view(), name='services'),
-    path('contato/', ContactView.as_view(), name='contact'),
-    path('eventos/', EventsView.as_view(), name='events'),
+    path('', views.home, name='home'),
+    path('servicos/', views.services, name='services'),
+    path('eventos/', views.events, name='events'),
+    path('contato/', views.contact, name='contact'),
 ]
